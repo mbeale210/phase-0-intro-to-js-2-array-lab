62ad242b5dcad81ef514dfc7f2be2cbaa04d51fb
Mark Beale <bealebeaverst@gmail.com>
2024-07-02 15:45:02 -0700

    cats come and go

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(){
+    const appendCat = [ ...cats, "Broom"]
+    return appendCat
+}
+function prependCat(){
+    const prependCat = ["Arnold", ...cats]
+    return prependCat
+}
+function removeLastCat(){
+    let newCats = [...cats];
+    newCats.pop();
+    return newCats;
+}
+function removeFirstCat(){
+    let newCats = cats.slice(0);
+    newCats.shift();
+    return newCats;
+}
